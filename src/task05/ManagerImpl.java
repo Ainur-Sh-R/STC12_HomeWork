e901820e68f5b150cd84e52fcb209d69fb5509cb
Ainur-Sh-R <a.shigapov.stc@innopolis.university>
2018-08-30 01:13:27 +0300

    добавлено описание методов

--- src/task05/ManagerImpl.java
+++ src/task05/ManagerImpl.java
@@ -9,6 +9,11 @@ import java.util.List;
 public class ManagerImpl implements Manager {
     private File file;
 
+    /**
+     * конструктор класса в атрибутах принимает файл
+     * в потоке записи outStream в файл записывается пустой список сотрудников
+     * @param file
+     */
     public ManagerImpl(File file) {
         this.file = file;
         try (ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream(file, false))) {
@@ -18,6 +23,14 @@ public class ManagerImpl implements Manager {
         }
     }
 
+    /**
+     * производится считываение списка сотрудников employees с файла, потоком inStream
+     * поток записи outStream  дописывает сотрудника в конец файла, при наличии сотрудника в
+     * списке запись не производится
+     * @param employee - сотрудник
+     * @return true, если сотрудник отсутсвует в списке
+     *          false, при наличии сторудника в списке
+     */
     @Override
     public boolean save(Employee employee) {
         try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(file));
@@ -36,6 +49,13 @@ public class ManagerImpl implements Manager {
         return false;
     }
 
+
+    /**
+     * метод удаляет сотрудника из списка, при его налачия в списке
+     * @param employee - сотрудник
+     * @return true, если сотрудник есть в списке, производится уделение из спика
+     *          false, сотрудник отсутвует в списке
+     */
     @Override
     public boolean delete(Employee employee) {
         try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(file));
@@ -52,6 +72,11 @@ public class ManagerImpl implements Manager {
         return false;
     }
 
+    /**
+     * возвращает сотрудника по полному совпадению имени
+     * при отсутствии сотрудника в списке, возвращается null
+     * @param name - имя сотрудника
+     */
     @Override
     public Employee getByName(String name) {
         try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(file))) {
@@ -67,6 +92,11 @@ public class ManagerImpl implements Manager {
         return null;
     }
 
+    /**
+     *
+     * @param job - должность
+     * @return список сотрудников с указанной должностью
+     */
     @Override
     public List<Employee> getByJob(Job job) {
         try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(file))) {
@@ -83,6 +113,12 @@ public class ManagerImpl implements Manager {
         return null;
     }
 
+    /**
+     * выполняет обновление, либо сохранение сотрудника
+     * в зависимости от того, есть ли он уже в файле
+     * @return true, если сотрудник есть в спике и происходит его обновление
+     *      false, если сотрудника нет в списке, то происходит сохранение в список сотрудника
+     */
     @Override
     public boolean saveOrUpdate(Employee employee) {
         try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(file));
@@ -102,6 +138,10 @@ public class ManagerImpl implements Manager {
         return false;
     }
 
+    /**
+     * замена должности для всех сотрудников на заданную
+     * @return успешность замены должности
+     */
     @Override
     public boolean changeAllWork(Job job) {
         try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(file));
@@ -118,6 +158,10 @@ public class ManagerImpl implements Manager {
         return false;
     }
 
+    /**
+     * метод производит запись в файл исключения возникшие при работе программы
+     * @param message исключение с описанием возникшей ошибки в ходе работы программы
+     */
     private void log(String message) {
         System.out.println(message);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("log.txt"), true))) {
